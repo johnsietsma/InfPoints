48c3276ff708bfabaccf3d528151322e87268997
John Sietsma <john@sietsma.com>
2020-01-05 18:20:11 +1100

    Early Octree implementation

--- com.infpoints/Runtime/NativeSparseOctree.cs
+++ com.infpoints/Runtime/NativeSparseOctree.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using UnityEngine.Assertions;
 
 namespace InfPoints
 {
@@ -13,47 +15,93 @@ namespace InfPoints
     ///   belong to a node.
     /// </summary>
     [NativeContainer]
-    public struct NativeSparseOctree<T> : IDisposable where T: unmanaged
+    public unsafe struct NativeSparseOctree<T> : IDisposable where T : unmanaged
     {
+        const int MaximumLevelCount = 7; // Based on morton code limits
+
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
         AtomicSafetyHandle m_Safety;
 
         [NativeSetClassTypeToNullOnSchedule] DisposeSentinel m_DisposeSentinel;
         static readonly int DisposeSentinelStackDepth = 2;
 #endif
-        
-        public bool IsCreated => m_Levels.IsCreated;
-        
+        public bool IsCreated { get; private set; }
         public int LevelCount { get; private set; }
 
-
         Allocator m_Allocator;
-        NativeArray<NativeSparseArray<T>> m_Levels;
+        
+        NativeSparseList<T> m_Level0;
+        NativeSparseList<T> m_Level1;
+        NativeSparseList<T> m_Level2;
+        NativeSparseList<T> m_Level3;
+        NativeSparseList<T> m_Level4;
+        NativeSparseList<T> m_Level5;
+        NativeSparseList<T> m_Level6;
 
-        public NativeSparseOctree(int maxLevelCount, Allocator allocator)
+        public NativeSparseOctree(Allocator allocator)
         {
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
             // Native allocation is only valid for Temp, Job and Persistent.
             if (allocator <= Allocator.None)
                 throw new ArgumentException("Allocator must be Temp, TempJob or Persistent", nameof(allocator));
-            if (maxLevelCount < 0)
-                throw new ArgumentOutOfRangeException(nameof(maxLevelCount), "Must be >= 0");
-            
+
             DisposeSentinel.Create(out m_Safety, out m_DisposeSentinel, DisposeSentinelStackDepth, allocator);
             AtomicSafetyHandle.SetBumpSecondaryVersionOnScheduleWrite(m_Safety, true);
 #endif
             m_Allocator = allocator;
+            var level0 = new NativeSparseList<T>(OctreeUtils.GetNodeCount(0), allocator);
+            //m_ppLevels = &(level0.GetUnsafePointer());
+            
             LevelCount = 0;
-            m_Levels = new NativeArray<NativeSparseArray<T>>(maxLevelCount, allocator);
+            m_Level0 = default;
+            m_Level1 = default;
+            m_Level2 = default;
+            m_Level3 = default;
+            m_Level4 = default;
+            m_Level5 = default;
+            m_Level6 = default;
+            IsCreated = true;
         }
 
-        public void AddLevel(int maxNodeCountForLevel)
+        public void AddLevel()
         {
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
-            if( LevelCount==m_Levels.Length-1 )
+            if (LevelCount == MaximumLevelCount - 1)
                 throw new ArgumentOutOfRangeException();
-#endif            
+#endif
+            int nodeCount = OctreeUtils.GetNodeCount(LevelCount);
+            var initialCapacity = math.min(1024, nodeCount);
+            switch (LevelCount)
+            {
+                case 0:
+                    m_Level0 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                case 1:
+                    m_Level1 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                case 2:
+                    m_Level2 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                case 3:
+                    m_Level3 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                case 4:
+                    m_Level4 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                case 5:
+                    m_Level5 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                case 6:
+                    m_Level6 = new NativeSparseList<T>(initialCapacity, Allocator.Persistent);
+                    break;
+                default:
+                    Assert.IsTrue(false, "Internal error");
+                    break;
+            }
+
+            LevelCount++;
         }
+
 
         public void Dispose()
         {
@@ -62,7 +110,14 @@ namespace InfPoints
 #if ENABLE_UNITY_COLLECTIONS_CHECKS
             DisposeSentinel.Dispose(ref m_Safety, ref m_DisposeSentinel);
 #endif
-            m_Levels.Dispose();
+            if (m_Level0.IsCreated) m_Level0.Dispose();
+            if (m_Level1.IsCreated) m_Level1.Dispose();
+            if (m_Level2.IsCreated) m_Level2.Dispose();
+            if (m_Level3.IsCreated) m_Level3.Dispose();
+            if (m_Level4.IsCreated) m_Level4.Dispose();
+            if (m_Level5.IsCreated) m_Level5.Dispose();
+            if (m_Level6.IsCreated) m_Level6.Dispose();
+            IsCreated = false;
         }
     }
 }
---
+++ com.infpoints/Runtime/OctreeUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public static class OctreeUtils
+{
+    /// <summary>
+    /// Return the number of nodes in the given level 
+    /// </summary>
+    /// <param name="levelIndex"></param>
+    /// <returns>The number of nodes in the level</returns>
+    public static int GetNodeCount(int levelIndex)
+    {
+        return (int)math.pow(8, levelIndex);
+    }
+}
