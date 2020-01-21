b149328d09981260c5a943cb882d7ef3b11d3627
John Sietsma <js656@newcastle.edu.au>
2020-01-22 08:59:08 +1100

    Move point cloud job scheduling to a static class

--- com.infpoints/Runtime/PointCloud.cs
+++ com.infpoints/Runtime/PointCloud.cs
@@ -1,5 +1,4 @@
 ﻿using Unity.Collections;
-using Unity.Jobs;
 using Unity.Mathematics;
 
 namespace InfPoints
@@ -9,7 +8,6 @@ namespace InfPoints
         SparseOctree<int> m_Octree;
         NativeHashMap<int, int> m_PointStorage;
 
-        const int InnerLoopBatchCount = 1024;
 
         public PointCloud(AABB aabb)
         {
@@ -29,74 +27,13 @@ namespace InfPoints
             int cellCount = SparseOctree<int>.GetCellCount(levelIndex);
             float cellWidth = m_Octree.AABB.Size / cellCount;
 
-            var transformHandle = TransformPoints(pointsWide, -m_Octree.AABB.Minimum);
-            var pointsToCoordinatesHandle = PointsToCoordinates(pointsWide, coordinatesWide, cellWidth, transformHandle);
-            var mortonCodeHandle = CoordinatesToMortonCode(coordinates, codes, pointsToCoordinatesHandle);
+            var transformHandle = PointCloudJobs.ScheduleTransformPoints(pointsWide, -m_Octree.AABB.Minimum);
+            var pointsToCoordinatesHandle = PointCloudJobs.SchedulePointsToCoordinates(pointsWide, coordinatesWide, cellWidth, transformHandle);
+            var mortonCodeHandle = PointCloudJobs.ScheduleCoordinatesToMortonCode(coordinates, codes, pointsToCoordinatesHandle);
             
             coordinates.Dispose();
             codes.Dispose();
         }
-
-        static JobHandle TransformPoints(XYZSoA<float4> pointsWide, float3 numberToAdd)
-        {
-            // Convert points to Octree AABB space
-            var spaceConvertJobX = new AdditionJob_float4()
-            {
-                Values = pointsWide.X,
-                NumberToAdd = -numberToAdd.x
-            };
-            var spaceConvertJobY = new AdditionJob_float4()
-            {
-                Values = pointsWide.Y,
-                NumberToAdd = -numberToAdd.y
-            };
-            var spaceConvertJobZ = new AdditionJob_float4()
-            {
-                Values = pointsWide.Z,
-                NumberToAdd = -numberToAdd.z
-            };
-
-            return JobUtils.ScheduleMultiple(pointsWide.X.Length, InnerLoopBatchCount, spaceConvertJobX, spaceConvertJobY, spaceConvertJobZ);
-        }
-
-        static JobHandle PointsToCoordinates(XYZSoA<float4> points, XYZSoA<uint4> coordinates, float divisionAmount, JobHandle dependsOn)
-        {
-            var convertJobX = new IntegerDivisionJob_float4_uint4()
-            {
-                Values = points.X,
-                Divisor = divisionAmount,
-                Quotients = coordinates.X
-            };
-            
-            var convertJobY = new IntegerDivisionJob_float4_uint4()
-            {
-                Values = points.Y,
-                Divisor = divisionAmount,
-                Quotients = coordinates.Y
-            };
-            
-            var convertJobZ = new IntegerDivisionJob_float4_uint4()
-            {
-                Values = points.Z,
-                Divisor = divisionAmount,
-                Quotients = coordinates.Z
-            };
-
-            return JobUtils.ScheduleMultiple(points.Length, InnerLoopBatchCount, dependsOn, convertJobX, convertJobY,
-                convertJobZ);
-        }
         
-        static JobHandle CoordinatesToMortonCode(XYZSoA<uint> coordinates, NativeArray<ulong> codes, JobHandle dependsOn)
-        {
-            var mortonEncodeJob = new Morton64SoAEncodeJob()
-            {
-                CoordinatesX = coordinates.X,
-                CoordinatesY = coordinates.Y,
-                CoordinatesZ = coordinates.Z,
-                Codes = codes
-            };
-
-            return mortonEncodeJob.Schedule(coordinates.Length, InnerLoopBatchCount, dependsOn);
-        }
     }
 }
---
+++ com.infpoints/Runtime/PointCloudJobs.cs
@@ -0,0 +1,76 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace InfPoints
+{
+    public static class PointCloudJobs
+    {
+        const int InnerLoopBatchCount = 1024;
+
+        public static JobHandle ScheduleTransformPoints(XYZSoA<float4> pointsWide, float3 numberToAdd)
+        {
+            // Convert points to Octree AABB space
+            var spaceConvertJobX = new AdditionJob_float4()
+            {
+                Values = pointsWide.X,
+                NumberToAdd = -numberToAdd.x
+            };
+            var spaceConvertJobY = new AdditionJob_float4()
+            {
+                Values = pointsWide.Y,
+                NumberToAdd = -numberToAdd.y
+            };
+            var spaceConvertJobZ = new AdditionJob_float4()
+            {
+                Values = pointsWide.Z,
+                NumberToAdd = -numberToAdd.z
+            };
+
+            return JobUtils.ScheduleMultiple(pointsWide.X.Length, InnerLoopBatchCount, spaceConvertJobX,
+                spaceConvertJobY, spaceConvertJobZ);
+        }
+
+        public static JobHandle SchedulePointsToCoordinates(XYZSoA<float4> points, XYZSoA<uint4> coordinates,
+            float divisionAmount, JobHandle dependsOn)
+        {
+            var convertJobX = new IntegerDivisionJob_float4_uint4()
+            {
+                Values = points.X,
+                Divisor = divisionAmount,
+                Quotients = coordinates.X
+            };
+
+            var convertJobY = new IntegerDivisionJob_float4_uint4()
+            {
+                Values = points.Y,
+                Divisor = divisionAmount,
+                Quotients = coordinates.Y
+            };
+
+            var convertJobZ = new IntegerDivisionJob_float4_uint4()
+            {
+                Values = points.Z,
+                Divisor = divisionAmount,
+                Quotients = coordinates.Z
+            };
+
+            return JobUtils.ScheduleMultiple(points.Length, InnerLoopBatchCount, dependsOn, convertJobX, convertJobY,
+                convertJobZ);
+        }
+
+        public static JobHandle ScheduleCoordinatesToMortonCode(XYZSoA<uint> coordinates, NativeArray<ulong> codes,
+            JobHandle dependsOn)
+        {
+            var mortonEncodeJob = new Morton64SoAEncodeJob()
+            {
+                CoordinatesX = coordinates.X,
+                CoordinatesY = coordinates.Y,
+                CoordinatesZ = coordinates.Z,
+                Codes = codes
+            };
+
+            return mortonEncodeJob.Schedule(coordinates.Length, InnerLoopBatchCount, dependsOn);
+        }
+    }
+}
