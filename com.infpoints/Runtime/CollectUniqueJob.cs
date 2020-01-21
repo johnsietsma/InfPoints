c747cfd93fc23e37b8e010df67292a2d04af89b1
John Sietsma <js656@newcastle.edu.au>
2020-01-22 09:15:21 +1100

    Add job to collect unique values from a NativeArray

---
+++ com.infpoints/Runtime/CollectUniqueJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace InfPoints
+{
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    public struct CollectUniqueJob<T> : IJob where T : unmanaged, IEquatable<T>
+    {
+        [ReadOnly] public NativeArray<T> Values;
+        public NativeHashMap<T, int> UniqueValues;
+
+        public void Execute()
+        {
+            for (int index = 0; index < Values.Length; index++)
+            {
+                if (!UniqueValues.ContainsKey(Values[index]))
+                {
+                    UniqueValues.Add(Values[index], 1);
+                }
+            }
+        }
+    }
+}
---
+++ com.infpoints/Tests/Editor/CollectUniqueJobTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace InfPoints.Tests.Editor
+{
+    public class CollectUniqueJobTests
+    {
+        [Test]
+        public void DoesCollectUniqueValues()
+        {
+            int[] values = {1, 1, 2, 3, 5, 5, 6};
+            using (var valuesArray = new NativeArray<int>(values, Allocator.TempJob))
+                using( var uniqueMap = new NativeHashMap<int,int>(valuesArray.Length, Allocator.TempJob))
+                {
+                    var uniqueJob = new CollectUniqueJob<int>()
+                    {
+                        Values = valuesArray,
+                        UniqueValues = uniqueMap
+                    };
+
+                    var jobHandle = uniqueJob.Schedule();
+                    jobHandle.Complete();
+
+                    Assert.That(uniqueMap.Length, Is.EqualTo(5));
+                    Assert.That(uniqueMap.ContainsKey(5), Is.True);
+                }
+        }
+        
+    }
+}
