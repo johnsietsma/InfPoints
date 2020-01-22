168b7f68f5bc30fd3885286ff1d5c44aa6243324
John Sietsma <js656@newcastle.edu.au>
2020-01-22 15:18:22 +1100

    Added a job to get NativeHashMapValues

--- com.infpoints/Runtime/JobUtils.cs
+++ com.infpoints/Runtime/JobUtils.cs
@@ -1,6 +1,5 @@
 ﻿using Unity.Collections;
 using Unity.Jobs;
-using UnityEditor;
 
 namespace InfPoints
 {
--- com.infpoints/Tests/Editor/CollectUniqueJobTests.cs
+++ com.infpoints/Tests/Editor/CollectUniqueJobTests.cs
@@ -11,21 +11,38 @@ namespace InfPoints.Tests.Editor
         {
             int[] values = {1, 1, 2, 3, 5, 5, 6};
             using (var valuesArray = new NativeArray<int>(values, Allocator.TempJob))
-                using( var uniqueMap = new NativeHashMap<int,int>(valuesArray.Length, Allocator.TempJob))
+            using (var uniqueMap = new NativeHashMap<int, int>(valuesArray.Length, Allocator.TempJob))
+            {
+                var uniqueJob = new CollectUniqueJob<int>()
                 {
-                    var uniqueJob = new CollectUniqueJob<int>()
-                    {
-                        Values = valuesArray,
-                        UniqueValues = uniqueMap
-                    };
+                    Values = valuesArray,
+                    UniqueValues = uniqueMap
+                };
 
-                    var jobHandle = uniqueJob.Schedule();
-                    jobHandle.Complete();
 
-                    Assert.That(uniqueMap.Length, Is.EqualTo(5));
-                    Assert.That(uniqueMap.ContainsKey(5), Is.True);
-                }
-        }
+                var uniqueValues = new NativeList<int>(uniqueMap.Length, Allocator.Persistent);
+
+                var toArrayJob = new NativeHashMapGetValuesJob<int>()
+                {
+                    UniqueHash = uniqueMap,
+                    UniqueValues = uniqueValues
+                };
+
+                
+                var collectUniqueJobHandle = uniqueJob.Schedule();
+                collectUniqueJobHandle.Complete();
+
+                // Must complete the collect job before scheduling so we can use the results
+                var toArrayJobHandle = toArrayJob.Schedule();
+                toArrayJobHandle.Complete();
         
+                Assert.That(uniqueMap.Length, Is.EqualTo(5));
+                Assert.That(uniqueMap.ContainsKey(5), Is.True);
+                Assert.That(uniqueValues.Length, Is.EqualTo(5));
+                Assert.That(uniqueValues[3], Is.EqualTo(5));
+
+                uniqueValues.Dispose();
+            }
+        }
     }
 }
