ba8bc583be377b2a13ba3d7b734c78b97be43aa9
John Sietsma <js656@newcastle.edu.au>
2020-02-05 16:29:14 +1100

    Add tests for NativeInterlockedInt

---
+++ com.infpoints/Tests/Editor/NativeCollections/NativeInterlockedIntTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace InfPoints.Tests.Editor.NativeCollections
+{
+    internal struct IncrementInterlockedIntJob : IJobParallelFor
+    {
+        public NativeInterlockedInt Count;
+        
+        public void Execute(int index)
+        {
+            Count.Increment();
+        }
+    }
+    
+    public class NativeInterlockedIntTests
+    {
+        [Test]
+        public void AddingGivesTheCorrectResult()
+        {
+            using (NativeInterlockedInt interlockedInt = new NativeInterlockedInt(0, Allocator.TempJob))
+            {
+                Assert.That(interlockedInt.Value, Is.EqualTo(0));
+                interlockedInt.Add(5);
+                Assert.That(interlockedInt.Value, Is.EqualTo(5));
+            }
+        }
+        
+        [Test]
+        public void IncrementingGivesTheCorrectResult()
+        {
+            using (NativeInterlockedInt interlockedInt = new NativeInterlockedInt(10, Allocator.TempJob))
+            {
+                Assert.That(interlockedInt.Value, Is.EqualTo(10));
+                interlockedInt.Increment();
+                Assert.That(interlockedInt.Value, Is.EqualTo(11));
+            }
+        }
+        
+        [Test]
+        public void DecrementingGivesTheCorrectResult()
+        {
+            using (NativeInterlockedInt interlockedInt = new NativeInterlockedInt(0, Allocator.TempJob))
+            {
+                Assert.That(interlockedInt.Value, Is.EqualTo(0));
+                interlockedInt.Decrement();
+                Assert.That(interlockedInt.Value, Is.EqualTo(-1));
+            }
+        }
+
+        [Test]
+        public void IncrementGivesCorrectResultInJob()
+        {
+            const int IncrementCount = 1024 * 1024;
+            const int BatchCount = 2;
+            
+            using (NativeInterlockedInt interlockedInt = new NativeInterlockedInt(0, Allocator.TempJob))
+            {
+                var incrementJob = new IncrementInterlockedIntJob()
+                {
+                    Count = interlockedInt
+                }.Schedule(IncrementCount, BatchCount);
+                incrementJob.Complete();
+                
+                Assert.That(IncrementCount, Is.EqualTo(interlockedInt.Value));
+            }
+        }
+    }
+}
