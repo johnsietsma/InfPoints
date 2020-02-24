3de76e3bb02f6ecaa7b927e9d0a4e411a543acb4
John Sietsma <js656@newcastle.edu.au>
2020-02-24 17:26:27 +1100

    Add DBSCAN clustering jobs and tests

---
+++ com.infpoints/Runtime/Jobs/DBSCANClusteringJobs.cs
@@ -0,0 +1,60 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace InfPoints.Jobs
+{
+    //[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    public struct SetClustersJob : IJobParallelFor
+    {
+        [ReadOnly] NativeArray<float3> Data;
+        NativeArray<BitField32> Clusters;
+        [ReadOnly] float EpsilonSquared;
+        float3 Point;
+        [NativeDisableParallelForRestriction]
+        NativeInt Cluster;
+
+        public SetClustersJob(NativeArray<float3> data, NativeArray<BitField32> clusters, float epsilon, float3 point,
+            NativeInt cluster)
+        {
+            Data = data;
+            Clusters = clusters;
+            EpsilonSquared = math.pow(epsilon, 2);
+            Point = point;
+            Cluster = cluster;
+        }
+
+        public void Execute(int index)
+        {
+            if (math.distancesq(Data[index], Point) <= EpsilonSquared)
+            {
+                var clusters = Clusters[index];
+                clusters.Value |= (uint)Cluster.Value;
+                
+                // Both belong to the same clusters
+                Clusters[index] = clusters;
+                Cluster.Value = (int)clusters.Value;
+            }
+        }
+    }
+
+    [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+    public struct AggregateClustersJob : IJobParallelFor
+    {
+        NativeArray<BitField32> DataClusters;
+        [ReadOnly] BitField32 Cluster;
+        [ReadOnly] int ClusterPosition;
+
+        public void Execute(int index)
+        {
+            // The point is in our cluster
+            if (DataClusters[index].TestAll(ClusterPosition))
+            {
+                // Nuke the all the clusters but ours
+                var clusters = DataClusters[index];
+                clusters.Value &= ~(Cluster.Value | Cluster.Value);
+            }
+        }
+    }
+}
---
+++ com.infpoints/Tests/Editor/Jobs/DBSCANClusteringJobsTests.cs
@@ -0,0 +1,52 @@
+﻿using InfPoints.Jobs;
+using NUnit.Framework;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace InfPoints.Tests.Editor.Jobs
+{
+    public class DBSCANClusteringJobsTests
+    {
+        [Test]
+        public void SetClustersReturnsTheCorrectResult()
+        {
+            float3[] dataValues = new[]
+            {
+                float3.zero, 
+                new float3(1,0,0), 
+                new float3(0,0,2),
+            };
+            
+            BitField32[] clusterValues = new[]
+            {
+                new BitField32(1<<2),  
+                new BitField32(1<<3),  
+                new BitField32(1<<6),  
+            };
+
+            using (var data = new NativeArray<float3>(dataValues, Allocator.TempJob))
+            using (var clusters = new NativeArray<BitField32>(clusterValues, Allocator.TempJob))
+            using(var cluster = new NativeInt(1<<5, Allocator.TempJob))
+            {
+                new SetClustersJob(data, clusters, 1, float3.zero, cluster).Schedule(data.Length, 4).Complete();
+                
+                var clusterBits = new BitField32((uint)cluster.Value);
+                
+                Assert.That(clusterBits.IsSet(2), Is.True);
+                Assert.That(clusterBits.IsSet(3), Is.True);
+                Assert.That(clusterBits.IsSet(5), Is.True);
+                
+                Assert.That(clusters[0].IsSet(5), Is.True);
+                Assert.That(clusters[0].IsSet(2), Is.True);
+                Assert.That(clusters[1].IsSet(5), Is.True);
+                Assert.That(clusters[2].IsSet(5), Is.False);
+
+                int lowestPosition = clusterBits.CountTrailingZeros();
+                Assert.That(lowestPosition, Is.EqualTo(2));
+            }
+        }
+        
+        
+    }
+}
